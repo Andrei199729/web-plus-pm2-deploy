9a81c8e1e75a398f518a728266da266ec2e54d92
Andrei <Abdrei19972910@yandex.by>
2024-02-22 11:39:30 +0300

    Исправление линтера

--- backend/src/middlewares/check-password.ts
+++ backend/src/middlewares/check-password.ts
@@ -1,11 +1,11 @@
-import { RequestHandler } from 'express';
-import BadRequest from '../errors/bad-request-error';
+import { RequestHandler } from "express";
+import BadRequest from "../errors/bad-request-error";
 
 const checkPassword: RequestHandler = (req, res, next) => {
   const { password } = req.body;
 
   if (!password || !password.trim()) {
-    next(new BadRequest('Поле "password" должно быть заполнено'));
+    next(new BadRequest("Поле 'password' должно быть заполнено"));
   } else {
     next();
   }
